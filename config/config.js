c2e14a97cead856db6d161e9887d336e13205888
Rohitchavan2124 <r.rohitchavan724@gmail.com>
2023-04-10 18:16:42 +0530

    first commit

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/codcial');
+
+var db= mongoose.connection
+
+db.once('open',function(){
+    console.log('Db connected');
+})
+
+db.on('error',console.error.bind(console,'something happended'));
